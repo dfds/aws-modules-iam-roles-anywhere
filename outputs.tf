bebaa9c7e13c561c9fba27ae95b34f8b3d4bc04d
John Smith <josmi@dfds.com>
2023-02-28 10:27:27 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,14 @@
 output "iam_profile_arn" {
-  description = " IAM roles anywhere profile Arn"
+  description = "The Arn of the aws iam role anywhere profile"
   value       = aws_rolesanywhere_profile.this.arn
 }
 
 output "iam_role_arn" {
-  description = " IAM role Arn"
+  description = "The Arn of the aws iam role"
   value       = aws_iam_role.this.arn
 }
 
 output "trust_anchor_arn" {
+  description = "The Arn of the aws iam role anywhere trust anchor"
   value = aws_rolesanywhere_trust_anchor.this.arn
 }
